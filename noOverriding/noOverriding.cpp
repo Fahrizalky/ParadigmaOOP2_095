e376a6b8380501b4b3a14e29850e2c669875e30a
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-31 18:57:49 +0700

    membuat base class dari program pewarisan

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan() {
+	virtual void perkenalan()  {
 		cout << "Hallo saya Function dari base Class";
 	}
 };
 
-class derivedClass : public baseClass {
+class derivedClass: public baseClass {
 public:
 	void perkenalan() {
 		cout << "Hallo saya Function dari derived Class";
