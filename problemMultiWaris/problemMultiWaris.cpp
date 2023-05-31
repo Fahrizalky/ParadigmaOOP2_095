cf0f72e9b085a0fab6f7a31e504bfbb23c2e91b4
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-31 11:15:38 +0700

    membuat class orang

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 class orang {
-private:
+public:
 	int umur;
 
 	orang(int pUmur) :
 		umur(pUmur)
 	{
-		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 	}
 };
+
