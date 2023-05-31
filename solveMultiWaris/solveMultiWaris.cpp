ef589316a081d96a0f7b8194325bdf43a42a133c
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-31 17:51:31 +0700

    membuat class budi dari class pekerja dan pelajar dan disertai objek pUmur dari class pekerja dan pelajar

--- solveMultiWaris/solveMultiWaris.cpp
+++ solveMultiWaris/solveMultiWaris.cpp
@@ -32,3 +32,15 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur) //hal ini dapat dilakukan jiak menggunakan virtual
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
